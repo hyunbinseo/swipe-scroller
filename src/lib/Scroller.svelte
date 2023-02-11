dd14deff8c116e858aa4f8f8de5c9a27dafe58b3
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2023-02-11 21:50:57 +0900

    feat: implement scroller component

---
+++ src/lib/Scroller.svelte
@@ -0,0 +1,158 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import Chevron from './Chevron.svelte';
+
+	export let buttonWidth = '2.5rem';
+	export let cardGap = '1.25rem';
+	export let hangButtons = true;
+	export let invertButtons = false;
+
+	// Enable scroller
+
+	let referenceElement: HTMLElement;
+	let offsetLeft: number;
+
+	const updateOffsetLeft = () => (offsetLeft = referenceElement?.offsetLeft);
+
+	onMount(updateOffsetLeft);
+
+	// Scroll with button
+
+	let scroller: HTMLDivElement;
+
+	const scroll = (left = true) => {
+		const width = scroller.firstElementChild?.getBoundingClientRect().width;
+
+		if (!width) return;
+
+		const directionMultiplier = (left ? -1 : 1) * (invertButtons ? -1 : 1);
+
+		scroller.scrollLeft += directionMultiplier * width;
+	};
+</script>
+
+<svelte:window on:resize={updateOffsetLeft} />
+
+<div class="scroller-wrapper">
+	<div class="container-inner">
+		<slot name="noscript">
+			<noscript>JavaScript is not available. Horizontal scroll is disabled.</noscript>
+		</slot>
+		<div bind:this={referenceElement} />
+	</div>
+
+	<div class:container-inner={offsetLeft === undefined}>
+		<div
+			bind:this={scroller}
+			class="scroller"
+			class:scroller-js={offsetLeft !== undefined}
+			style="--slide-gap: {cardGap}; --scroller-offset: {offsetLeft || 0}px;"
+		>
+			<slot />
+		</div>
+	</div>
+
+	{#if offsetLeft !== undefined}
+		<div
+			class="container-outer scroller-control"
+			style="--button-width: {buttonWidth}; --button-margin-multiplier: {hangButtons ? -1 : 1}"
+		>
+			<button type="button" on:click={() => scroll()}>
+				<slot name="button-prev">
+					<div class="icon-wrapper" style="margin-left: var(--button-margin);">
+						<Chevron />
+					</div>
+				</slot>
+			</button>
+			<button type="button" on:click={() => scroll(false)}>
+				<slot name="button-next">
+					<div class="icon-wrapper" style="margin-right: var(--button-margin);">
+						<Chevron left={false} />
+					</div>
+				</slot>
+			</button>
+		</div>
+	{/if}
+</div>
+
+<style>
+	noscript {
+		display: block;
+		margin-bottom: 0.625rem;
+	}
+
+	.scroller-wrapper {
+		position: relative;
+	}
+
+	.scroller {
+		display: flex;
+		gap: var(--slide-gap);
+		padding: 0 var(--scroller-offset);
+		scroll-padding-left: var(--scroller-offset);
+		overflow-x: visible;
+	}
+
+	.scroller-js {
+		overflow-x: auto;
+		scroll-behavior: smooth;
+		scroll-snap-type: x mandatory;
+		scrollbar-width: none;
+	}
+
+	.scroller-js::-webkit-scrollbar {
+		display: none;
+	}
+
+	.scroller-control {
+		pointer-events: none;
+		position: absolute;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		display: flex;
+		justify-items: center;
+		justify-content: space-between;
+		opacity: 0;
+	}
+
+	.scroller-control > button {
+		all: unset;
+		pointer-events: auto;
+	}
+
+	.icon-wrapper {
+		border-radius: 100%;
+		height: var(--button-width);
+		width: var(--button-width);
+		padding: calc(var(--button-width) / 4);
+		background-color: rgba(0, 0, 0, 0.2);
+		color: white;
+		--button-margin: calc(var(--button-width) / 2);
+	}
+
+	@media (min-width: 640px) {
+		.icon-wrapper {
+			--button-margin: calc(var(--button-width) / 2 * var(--button-margin-multiplier));
+		}
+	}
+
+	@media (hover: hover) and (pointer: fine) {
+		.scroller-wrapper:hover .scroller-control {
+			opacity: 1;
+		}
+		.icon-wrapper:hover {
+			background-color: rgba(0, 0, 0, 0.4);
+		}
+	}
+
+	@media (prefers-reduced-motion: no-preference) {
+		.scroller-control {
+			transition: opacity 150ms;
+		}
+		.icon-wrapper {
+			transition: background-color 150ms;
+		}
+	}
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -9,6 +9,9 @@ const config = {
 
 	kit: {
 		adapter: adapter()
+	},
+	package: {
+		exports: (filepath) => filepath === 'Scroller.svelte'
 	}
 };
 
