826f2bdc985b9b94d23e87f3ea5a9b9360eaaa04
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2023-02-11 22:12:30 +0900

    chore: allow ./package vite fs

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		fs: {
+			allow: ['./package'] // Required to install and import locally built package.
+		}
+	}
 });
