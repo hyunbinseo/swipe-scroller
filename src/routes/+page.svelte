3847a247016465cd5acb30b1f7bb34db1e0b09b8
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2024-07-19 07:31:25 +0900

    chore: update demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,96 +1,62 @@
-<script>
+<script lang="ts">
 	import Scroller from '$lib/Scroller.svelte';
+	import '$lib/styles/container-colored.css';
 
-	const releases = [
-		{
-			name: 'Synthetic Heart',
-			color: 'rgb(172, 181, 186)',
-			spotify: '1uD8IR3fuDcIPWgvA1KS7c'
-		},
-		{
-			name: 'Your Eyes',
-			color: 'rgb(113, 120, 136)',
-			spotify: '1mTBvp0QIKjOD9KnxcFEDD'
-		},
-		{
-			name: 'Candid',
-			color: 'rgb(158, 128, 89)',
-			spotify: '27JqYhGZhbw4R64Ce7v1tg'
-		},
-		{
-			name: 'Yours, Truly',
-			color: 'rgb(124, 104, 116)',
-			spotify: '4eB2YfdivTrZjzSeQ4NB23'
-		},
-		{
-			name: 'Affectionate',
-			color: 'rgb(40, 35, 49)',
-			spotify: '1YTj33nCCjZxaPc2RZIjE0'
-		},
-		{
-			name: 'Lay It On Me',
-			color: 'rgb(188, 128, 145)',
-			spotify: '7GbY6kNMYLAkJzxUcUq7n3'
-		}
-	];
+	export let data;
 
 	let hangButtons = true;
 	let invertButtons = false;
 </script>
 
-<div class="container-inner">
-	<h1>Swipe Scroller</h1>
-	<p>
-		Modern Slider. <a href="https://github.com/hyunbinseo/swipe-scroller#readme">GitHub</a>
-	</p>
-	<div class="line" />
-	<h2>xiihu</h2>
-	<p>Discography, from latest</p>
-</div>
+<div class="container-outer" style="position: fixed; inset: 0;" />
 
-<Scroller {hangButtons} {invertButtons} cardGap="0.5rem">
-	<!-- eslint-disable-next-line @typescript-eslint/no-unused-vars -->
-	{#each { length: releases.length } as _, index}
-		{@const reverseIndex = releases.length - 1 - index}
-		{@const release = releases[reverseIndex]}
-		{@const src = `https://scroller.hyunbin.page/${reverseIndex}.jpg`}
-		<a
-			class="card"
-			target="_blank"
-			rel="noreferrer"
-			href="https://open.spotify.com/album/{release.spotify}"
-			style:background-color={release.color}
-		>
-			<img {src} draggable="false" alt="" />
-			<div>
-				{release.name}
-			</div>
-		</a>
-	{/each}
-</Scroller>
-
-<div class="container-inner options">
-	<strong>Hover over the scroller to test these options.</strong>
-	<ul>
-		<li>
-			<label>
-				<input type="checkbox" bind:checked={hangButtons} />
-				Hang the arrow buttons at the inner container border.
-			</label>
-		</li>
-		<li>
-			<label>
-				<input type="checkbox" bind:checked={invertButtons} />
-				Invert the functions of the left and right arrow buttons.
-			</label>
-		</li>
-	</ul>
+<div style="position: relative; overflow-x: hidden;">
+	<div class="container-inner" style="margin-bottom: 1rem;">
+		<h1>Swipe Scroller</h1>
+		<p>
+			<span>Modern Slider.</span>
+			<a href="https://github.com/hyunbinseo/swipe-scroller#readme" target="_blank">GitHub</a>
+		</p>
+		<div class="line" />
+		<h2>xiihu</h2>
+		<p>Discography, from latest</p>
+	</div>
+	<Scroller {hangButtons} {invertButtons} cardGap="0.5rem">
+		{#each data.releases as release}
+			<a
+				class="card"
+				target="_blank"
+				rel="noreferrer"
+				href="https://open.spotify.com/album/{release.spotify}"
+				style:background-color={release.color}
+			>
+				<img src={release.cover} draggable="false" alt="" />
+				<div>{release.name}</div>
+			</a>
+		{/each}
+	</Scroller>
+	<div class="container-inner options">
+		<strong>Hover over the scroller to test these options.</strong>
+		<ul>
+			<li>
+				<label>
+					<input type="checkbox" bind:checked={hangButtons} />
+					Hang the arrow buttons at the inner container border.
+				</label>
+			</li>
+			<li>
+				<label>
+					<input type="checkbox" bind:checked={invertButtons} />
+					Invert the functions of the left and right arrow buttons.
+				</label>
+			</li>
+		</ul>
+	</div>
 </div>
 
 <style>
 	a.card {
 		text-decoration: none;
-		/* fixes :focus outline being clipped */
 		outline-offset: -2px;
 	}
 
@@ -103,14 +69,12 @@
 	}
 
 	.card > img {
-		/* fixes <a> being larger than <img> */
 		display: block;
-		aspect-ratio: 1;
-		width: 100%;
+		max-width: 100%;
 	}
 
 	.card > div {
-		padding: 1.5rem 1rem;
+		padding: 1rem;
 		color: white;
 		text-align: center;
 	}
@@ -125,27 +89,30 @@
 
 	:root {
 		padding: 3rem 0;
+		line-height: 1.5;
 	}
 
 	a,
+	a:active,
 	a:visited {
 		color: inherit;
 	}
 
 	h1,
-	h2 {
-		letter-spacing: -0.025em;
+	h2,
+	p {
+		margin: 0;
 	}
 
-	h1 + p,
-	h2 + p {
-		margin-block-start: -0.67rem;
+	h1 {
+		letter-spacing: -0.025em;
 	}
 
 	.line {
 		margin: 3rem 0;
 		width: 3rem;
-		border-bottom: solid;
+		border-style: solid;
+		border-width: 0.5px;
 	}
 
 	.options {
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,3 +1,51 @@
+import cover0 from './0.jpg';
+import cover1 from './1.jpg';
+import cover2 from './2.jpg';
+import cover3 from './3.jpg';
+import cover4 from './4.jpg';
+import cover5 from './5.jpg';
+
 // This file is not part of the template.
 // It is used to prerender the demo page.
 export const prerender = true;
+
+const releases = [
+	{
+		name: 'Lay It On Me',
+		cover: cover5,
+		color: '#BC8091',
+		spotify: '7GbY6kNMYLAkJzxUcUq7n3'
+	},
+	{
+		name: 'Affectionate',
+		cover: cover4,
+		color: '#282331',
+		spotify: '1YTj33nCCjZxaPc2RZIjE0'
+	},
+	{
+		name: 'Yours, Truly',
+		cover: cover3,
+		color: '#7C6874',
+		spotify: '4eB2YfdivTrZjzSeQ4NB23'
+	},
+	{
+		name: 'Candid',
+		cover: cover2,
+		color: '#9E8059',
+		spotify: '27JqYhGZhbw4R64Ce7v1tg'
+	},
+	{
+		name: 'Your Eyes',
+		cover: cover1,
+		color: '#717888',
+		spotify: '1mTBvp0QIKjOD9KnxcFEDD'
+	},
+	{
+		name: 'Synthetic Heart',
+		cover: cover0,
+		color: '#859FB0',
+		spotify: '1uD8IR3fuDcIPWgvA1KS7c'
+	}
+];
+
+export const load = () => ({ releases });
